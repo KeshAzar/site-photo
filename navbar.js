69ce7e27aadb47c696486ebf899b5db5f37b2ba6
Azargeth <doriandej@hotmail.fr>
2022-11-24 00:57:53 +0100

    Better Navbar

---
+++ navbar.js
@@ -0,0 +1,4 @@
+var tglbtn = document.getElementById("nav-burger");
+var navlst = document.getElementById("nav-liste")
+
+tglbtn.addEventListener('click', () => {navlst.classList.toggle("active-burger");});
